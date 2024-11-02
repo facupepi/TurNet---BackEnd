29eaaf7dfa829f669609a1225971dec889163a40
Facundo Pepino <fpepino@facultad.sanfrancisco.utn.edu.ar>
2024-11-02 10:20:52 -0300

    Primer Implementación de Inicio de Sesión + Registro

--- src/sequelize/models/client.model.js
+++ src/sequelize/models/client.model.js
@@ -7,7 +7,11 @@ module.exports = (sequelize) => {
             primaryKey: true,
             autoIncrement: true
         },
-        name: {
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        last_name: {
             type: DataTypes.STRING,
             allowNull: false
         },
